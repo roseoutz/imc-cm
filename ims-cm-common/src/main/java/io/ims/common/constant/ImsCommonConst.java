124969fe33bee965f32bed0348ff168103c28ab5
Kimdonggyuuuuu <102125580+Kimdonggyuuuuu@users.noreply.github.com>
2022-06-27 18:11:01 +0900

    ims project init

---
+++ ims-cm-common/src/main/java/io/ims/common/constant/ImsCommonConst.java
@@ -0,0 +1,19 @@
+package io.ims.common.constant;
+
+/**
+ * packageName    : io.ims.common.constant
+ * fileName       : ImsCommonConst
+ * author         : kimdonggyuuuuu
+ * date           : 2022/06/27
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/06/27        kimdonggyuuuuu       최초 생성
+ */
+public class ImsCommonConst {
+
+    private ImsCommonConst(){}
+
+    public static String COMMON_EMPTY_STRING = "";
+}
